567fd3a40f8f5ef964e46b8e6fd0945009c81b40
ihor-fesina <fesinaihor@gmail.com>
2025-07-14 11:32:55 +0200

    update links

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,7 +1,7 @@
 const LINKS = {
     email: 'mailto:fesinaihor@gmail.com',
-    linkedin: 'https://www.linkedin.com/in/fesinaihor@gmail.com',
-    github: 'https://github.com/fesinaihor',
+    linkedin: 'https://www.linkedin.com/in/ihor-fesina/',
+    github: 'https://github.com/ihor-fesina',
     portfolio: 'https://my-portfolio-fesina.vercel.app/',
 }
 export default LINKS
